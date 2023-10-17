8d6d217c583b0b19ed940cabb6e07f4aa9639b9a
Lyradd <madedaryl@gmail.com>
2023-10-17 12:00:51 +0800

    ETS PWEB

--- script.js
+++ script.js
@@ -1,59 +1,49 @@
 const ElementID = document.getElementById("result");
-const apiUrl = "http://159.223.51.203/api/bio";
+const Url = "http://159.223.51.203/api/bio";
 
 function Card(name, id) {
-  const outer = document.createElement("div");
-  outer.classList.add("col-lg-2", "col-md-3", "col-sm-12", "col-12", "mb-3");
-  outer.style.marginTop = "20px";
+  //styling cards
+  const cardHTML = `
+      <div class="col-lg-2 col-md-3 col-sm-12 col-12 mb-3" style="margin-top: 20px;">
+        <div class="card border-left-primary shadow h-100 py-3" style="border-radius: 5px;">
+          <div class="card-body">
+            <div class="row no-gutters align-items-center">
+              <div class="col mr-5">
+                <div class="text-xs font-weight-bold text-primary mb-1" style="margin-right: 10px;">
+                  ${name}
+                </div>
+                <div class="h6 mb-1 font-weight-normal text-black">${id}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
 
-  const cardStyle = document.createElement("div");
-  cardStyle.classList.add(
-    "card",
-    "border-left-primary",
-    "shadow",
-    "h-120",
-    "py-3"
-  );
-  cardStyle.style.borderRadius = "5px";
-
-  const body = document.createElement("div");
-  body.classList.add("card-body");
-
-  const content = document.createElement("div");
-  content.classList.add("row", "no-gutters", "align-items-center");
-
-  const column = document.createElement("div");
-  column.classList.add("col", "mr-5");
-
-  const title = document.createElement("div");
-  title.classList.add("text-xs", "font-weight-bold", "text-primary", "mb-1");
-  title.textContent = name;
-  title.style.marginRight = "8px";
-
-  const value = document.createElement("div");
-  value.classList.add("h6", "mb-1", "font-weight-normal", "text-black");
-  value.textContent = id;
-
-  column.appendChild(title);
-  column.appendChild(value);
-  content.appendChild(column);
-  body.appendChild(content);
-  cardStyle.appendChild(body);
-  outer.appendChild(cardStyle);
-  ElementID.appendChild(outer);
+  ElementID.innerHTML += cardHTML;
 }
-fetch(apiUrl)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Network response error");
+
+fetch(Url)
+  .then((response) =>
+    // menangkap response
+    {
+      if (!response.ok) {
+        //chek status
+        throw new Error("Network response error");
+      }
+      return response.json(); //return jika ok
     }
-    return response.json();
-  })
-  .then((data) => {
-    for (let index = 0; index < 20; index++) {
-      const name = data[index].name;
-      const id = data[index].id;
-      Card(name, id);
+  )
+  .then(
+    (
+      data //menangkap data json
+    ) => {
+      for (let index = 0; index < 11; index++) // karna ada 11 data dalam api
+      {
+        const name = data[index].name;
+        const id = data[index].id;
+        Card(name, id); //panggil fungsi cards
+      }
     }
-  })
-  .catch((error) => console.error("Error:", error));
+  )
+  .catch((error) => console.error("Error:", error)); //menangkap error
